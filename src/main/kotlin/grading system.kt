4d6d57c29a87326653d572048d6cb4deec55e2d2
eMobilis <fathiabdikadir09@gmail.com>
2023-03-15 12:40:50 +0300

    grading system

---
+++ src/main/kotlin/grading system.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    print ("Enter marks scored")
+
+    var marks:Int = readLine()!!.toInt()
+
+
+    if (marks >= 80){
+        print("A Congratulations")
+    }
+    else if (marks >= 70){
+        print("A- Good job")
+    }
+    else if (marks >= 60){
+        print("B Average")
+    }
+    else if (marks >= 50){
+        print("C Good trial can do better")
+    }
+    else if (marks <=49){
+        print("Below average retake the exam")
+    }
+}

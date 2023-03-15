72ddff98c6048996daaeb405a4f88cb18ec32ba4
eMobilis <fathiabdikadir09@gmail.com>
2023-03-15 12:28:10 +0300

    dialings

---
+++ src/main/kotlin/comp ex 2.kt
@@ -0,0 +1,53 @@
+fun main(args: Array<String>) {
+    print("Enter number")
+
+    var number:Int = readLine()!!.toInt()
+
+    if (number == 0){
+        print("sh20 1GB for 1hr")
+    }
+    else if (number == 1){
+        print("Data deals")
+    }
+    else if (number == 2){
+        print("Daily bundles")
+    }
+    else if (number == 3){
+       print("Weekly bundles")
+    }
+    else if (number == 4){
+        print("Go Monthly")
+    }
+    else if (number == 5){
+        print("No expiry")
+    }
+    else if (number == 6){
+        print("Video bundles")
+    }
+    else if (number == 7){
+        print("Okpa data")
+    }
+    else if (number == 8){
+        print("Lipa Mdogo")
+    }
+    else if (number == 9){
+        print("Data plus new")
+    }
+    else if (number == 10){
+        print("Hot minutes")
+    }
+    else {
+        print ("Invalid choice")
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
